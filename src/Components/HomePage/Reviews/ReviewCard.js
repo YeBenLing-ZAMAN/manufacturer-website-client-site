b9e5e95a242b6c3843dd1643e92a4dbf7c1d4629
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-22 16:53:15 +0600

    testimonial section added

--- src/Components/HomePage/Reviews/ReviewCard.js
+++ src/Components/HomePage/Reviews/ReviewCard.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
-const ReviewCard = () => {
+const ReviewCard = ({ review }) => {
     return (
-        <div>
-            <h1>MR.Been</h1>
-            <p>this is the time to purchase</p>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+        <div className="card-body">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem cupiditate illum modi neque. Cupiditate, sequi.</p>
+            <div className="flex items-center">
+                <div className="avatar">
+                    <div className="rounded-full ring ring-primary ring-offset-base-100 w-16 mr-5">
+                        <img className='' src={review.img} alt='' />
+                    </div>
+                </div>
+                <div>
+                    <h4 className='text-l'>{review.name}</h4>
+                    <p className=''>{review.location}</p>
+                </div>
+            </div>
+            <div className="rating">
+                <input readOnly type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input readOnly type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input readOnly type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input readOnly type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input readOnly type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            </div>
         </div>
-    );
+    </div>
+    )
 };
 
 export default ReviewCard;
--- src/Components/HomePage/Reviews/Reviews.js
+++ src/Components/HomePage/Reviews/Reviews.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReviewCard from './ReviewCard';
+import quoteIcon from '../../../images/reviews/quote.svg';
+import people1 from "../../../images/reviews/people1.png";
+import people2 from "../../../images/reviews/people2.png";
+import people3 from "../../../images/reviews/people3.png";
 
 const Reviews = () => {
+  const reviews =[
+    {
+        id:1,
+        name:"winson Herry",
+        review:' ',
+        location:"New York",
+        img:people1
+
+    },
+    {
+        id:2,
+        name:"winson Herry",
+        review:' ',
+        location:"New York",
+        img:people2
+
+    },
+    {
+        id:3,
+        name:"winson Herry",
+        review:' ',
+        location:"New York",
+        img:people3
+
+    },
+]
     return (
-        <>
-          <h1 className='text-5xl '> our product is the best....</h1>
-          <ReviewCard></ReviewCard>  
-        </>
+      <section className='my-28 p-10'>
+      <div className='flex justify-around items-center'>
+          <div>
+              <div><h4 className='text-xl text-primary font-bold'>Testimonial</h4></div>
+              <h2 className='text-3xl'>What Our Patients Says</h2>
+              <div></div>
+          </div>
+          <div>
+              <img className='w-24 lg:w-48' src={quoteIcon} alt="" />
+          </div>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      {
+          reviews.map(review=><ReviewCard key={review.id} review={review}></ReviewCard>)
+      }
+      </div>
+  </section>
     );
 };
 
