16fa0517742b20c9b1b2621f88984ebb2f40acbb
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-22 14:07:08 +0600

    simple Bannar page added'

--- src/Components/HomePage/Bannar.js
+++ src/Components/HomePage/Bannar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import bannar01 from "../../images/bannar/banner-01.jpg";
 
 const Bannar = () => {
     return (
-        <div>
-            this is a bannar
+        <div class="hero min-h-screen" style={{backgroundImage: `url(${bannar01})`}}>
+            <div class="hero-overlay bg-opacity-60"></div>
+            <div class="hero-content text-center text-neutral-content">
+                <div class="max-w-md">
+                    <h1 class="mb-5 text-5xl font-bold">High Quality Tools</h1>
+                    <p class="mb-5">Sanitary & building material Tools</p>
+                    <button class="btn btn-primary">purchase</button>
+                </div>
+            </div>
         </div>
     );
 };
