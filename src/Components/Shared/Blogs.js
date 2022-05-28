e7de89ea25f59691c7dc7d57a2a1e1d6484068f9
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-28 11:34:19 +0600

    blogs section added

--- src/Components/Shared/Blogs.js
+++ src/Components/Shared/Blogs.js
@@ -1,62 +1,75 @@
 import React from 'react';
-
+import blogCodeImage from '../../images/reviews/blogcode.jpg';
 const Blogs = () => {
     return (
-        <div className='p-5 m-5 border border-2 shadow-lg border-danger'>
+        <div className='p-5 m-5 border-2 shadow-lg border-primary'>
             <div>
-                <h3>Difference between javascript and nodejs</h3>
-                <p><strong>javascript:</strong> Javascript is a Programmign language. It's famous on website development.Javascript can only be run in the browsers.It is basically used on the client-side.Javascript is capable enough to add HTML and play with the DOM. Javascript can run in any browser engine as like JS core in safari and Spidermonkey in Firefox.
-                    <br />
-                    <strong>nodejs:</strong> NodeJS is a Javascript runtime environment.We can run Javascript outside the browser with the help of NodeJS.It is mostly used on the server-side.Nodejs does not have capability to add HTML tags.V8 is the Javascript engine inside of node.js that parses and runs Javascript. Nodejs is used in server-side development.</p>
+                <h3 className='text-2xl mb-2 font-bold'>How will you improve the performance of a React Application?</h3>
+                <p><span className='text-xl font-bold'>Answer: </span> Internally, React uses several clever techniques to minimize the number of costly DOM operations required to update the UI. While this will lead to a faster user interface without specifically optimizing for performance for many cases, there are ways where you can still speed up your React application. in blow list several steps: </p>
+                <div className='pl-10 mb-10'>
+                    <ul className='list-disc list-outside'>
+                        <li>Rollup</li>
+                        <li>Virtualize Long Lists</li>
+                        <li>shouldComponentUpdate In Action</li>
+                        <li>The Power Of Not Mutating Data</li>
+                        <li>Dependency optimization</li>
+                        <li>Multiple Chunk Files</li>
+                    </ul>
+                </div>
             </div>
             <hr />
             <div>
-                <h3> When should you use nodejs and when should you use mongodb?</h3>
-                <p>Choosing the right languages, tools and platforms can determine a lot of aspects of the future app functioning – from the straightforwardness of development to the performance and efficiency.Thus, it is critical to make the right choice in the beginning.If you have a web application project in mind, you are, most probably, evaluating the pros and cons of building your stack around JavaScript and the tools using it. Sooner or later, you will think of the backend development platform, and surely come across Node.js.</p>
-                <p>MongoDB is an open-source document database built on a horizontal scale-out architecture that uses a flexible schema for storing data. MongoDB is built on a scale-out architecture that has become popular with developers of all kinds for developing scalable applications with evolving data schemas.MongoDB's performance is much faster than any RDBMS. MongoDB is designed to work with Large Scale Data. MongoDB can work on multiple servers. MongoDB can handle a large number of access requests easily.</p>
-                <ul>
-                    <li>Document Model</li>
-                    <li>Get Started Quickly</li>
-                    <li>Deployment Options</li>
-                    <li>Find Community</li>
-                </ul>
+                <h3 className='text-2xl mb-2 font-bold'>What are the different ways to manage a state in a React application</h3>
+                <p><span className='text-xl font-bold'>Answer: </span> There are lots of manage a state in a react application.react state management is React components have a built-in state object. The state is encapsulated data where you store assets that are persistent between component renderings.</p>
+                <div className='pl-10 mb-10'>
+                    <ul className='list-disc list-outside'>
+                        <li>Local state => useReducer || useState || dispath</li>
+                        <li>Global state => useReducr || useContext</li>
+                        <li>Server state => useState || useEffect || useSWR || fetcher</li>
+                        <li>URL state => useHistory || useLocaton || useParams || seRouter</li>
+                    </ul>
+                </div>
             </div>
             <hr />
             <div>
-                <h3>Differences between sql and nosql databases?</h3>
-                <p></p>
-                <ul>
-                    <li><strong>sql databases</strong></li>
-                    <li>Structured query language (SQL)		</li>
-                    <li>Table	</li>
-                    <li>Predefined	</li>
-                    <li>ACID	</li>
-                    <li>Relational	</li>
-                    <li>Vertical	</li>
-                    <li>Specialized DB hardware (Oracle Exadata, etc.)		</li>
-                    <li>MySQL, PostgreSQL, Oracle, and MS-SQL		</li>
-                    <li>A mix of open-source like Postgres & MySQL, and commercial like Oracle Database.</li>
-                    <li>Cross-platform support, secure and free	</li>
-                </ul>
-                <ul>
-                    <li><strong>nosql databases</strong></li>
-                    <li>No declarative query language</li>
-                    <li>Key-value, document, wide-column, and graph</li>
-                    <li>Dynamic	</li>
-                    <li>Non-relational</li>
-                    <li>MongoDB, Apache HBase, Amazon DynamoDB, Redis, Couchbase, Cassandra, and Elasticsearch</li>
-                    <li>Horizontal</li>
-                    <li>MongoDB, Apache HBase, Amazon DynamoDB, Redis, Couchbase, Cassandra, and Elasticsearch</li>
-                    <li>Commodity hardware</li>
-                    <li>BASE</li>
-                    <li>Easy to use, high performance, and flexible tool</li>
-                </ul>
+                <h3 className='text-2xl mb-2 font-bold'>How does prototypical inheritance work?</h3>
+                <p><span className='text-xl font-bold'>Answer: </span> Simply put, prototypical inheritance refers to the ability to access object properties from another object. We use a JavaScript prototype to add new properties and methods to an existing object constructor.
+                </p>
+                <p>Prototypical inheritance clearly has a lot of benefits for JavaScript programmings, but, like all tools, it does have limitations. Let’s take a look at the key downsides to look out for as you write a prototype-based</p>
+                <p className='font-bold'>Program : </p>
+                <div className='pl-10 mb-10'>
+                    <ul className='list-disc list-outside'>
+                        <li> <span className='font-bold'>Inheritance cannot flow in circles as this will create an error.</span></li>
+                        <li><span className='font-bold'>Objects cannot inherit from multiple prototypes. </span>As we saw above, they can inherit multiple object’s properties through a chain, however another object linked as a prototype explicitly will cause an error. This is the case even if the additional prototype is</li>
+                        <li><span className='font-bold'>Prototypical relationships can only be made to objects.</span> This is because the __proto__ function works as a forwarder, directing the program where to find the value it is looking for. </li>
+                    </ul>
+                </div>
             </div>
             <hr />
             <div>
-                <h3> What is the purpose of jwt and how does it work?</h3>
-                <p></p>
+                <h3 className='text-2xl mb-4 font-bold'>You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</h3>
+                <div>
+                    <span className='text-xl font-bold'>Answer: </span>
+                    <img src={blogCodeImage} alt="" />
+                </div>
             </div>
+            <hr className='mt-6' />
+            <div>
+                <h3 className='text-2xl my-4 font-bold'>What is a unit test? Why should write unit tests?</h3>
+                <p><span className='text-xl font-bold'>Answer: </span> Unit test, also known as module test, refers to the test of the smallest testable unit in the software. The purpose is to check whether each unit can realize the function correctly and find out various defects that may exist in each module.
+                    <p>Integration test is a test which is based on unit test and according to the design requirements, the units that passes unit test are assembled into a system or subsystem. The purpose is to check whether the interface relationship between different program units or components meets the requirements and whether they can operate normally.</p>
+                </p>
+                <p>
+                    <span className='font-bold text-md'>During unit test, if the module is not a separate program, some auxiliary test modules need to be set. There are two types of auxiliary test modules:</span>
+                    <div className='pl-10 mb-10'>
+                        <ul className='list-disc list-outside'>
+                            <li>Driver: Simulate the upper module of the tested module, which is equivalent to the main program of the tested module. It receives the data, transmits the relevant data to the tested module, starts the tested module, and produces the corresponding results.</li>
+                            <li>Stub: It is used to simulate the module called in the working process of the tested module.</li>
+                        </ul>
+                    </div>
+                </p>
+            </div>
+            <hr />
         </div>
     );
 };
